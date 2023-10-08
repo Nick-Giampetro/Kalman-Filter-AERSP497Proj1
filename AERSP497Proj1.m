4b407720d9584c378967ec0c7d03d7d2a4ae08a8
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-10-08 17:38:13 -0400

    loading in the HW solution actually

--- AERSP497Proj1.m
+++ AERSP497Proj1.m
@@ -1,87 +1,101 @@
 
-% KF 2
-close all
-clear all
-% time parameters
-dT=0.1;
-t0=0;
-tf=20;
-%define filter parameters
-xhat0=[0;0];
-P0=eye(2);
-Q_extra=0*[1 0;0 1]; % fictitious process noise
-R=0.1; % assumed measurement noise
-% define true initial conditions
-x0=[2;0];
-% define model parameters
-m=1;
-b=0.4;
-k=1;
-% define noise parameters
-u_bar=0;
-S_u=0.2;
-S_v=0.1;
-% measurement of position only
-C=[1 0];
-dk=0; % potential for unmodelled plant error...
-db=0; % -0.3;
-% continuous time dynamics
-A_ct=[0 1;-(k+dk)/m -(b+db)/m]; % true dynamics
-A_cm=[0 1;-(k)/m -b/m]; % assumed dynamics
-B_c=[0;1/m];
-% convert to discrete time using ZOH
-A_true=expm(A_ct*dT);
-A_model=expm(A_cm*dT);
-B_true=(A_true-eye(2))*inv(A_ct)*B_c;
-B_model=(A_model-eye(2))*inv(A_cm)*B_c;
-% Q=B'Su*B
-Q_model=B_model*S_u*B_model';
-time=t0:dT:tf;
-MAXK=length(time);
-x_tru(:,1)=x0; % initialize truth
-x_hat(:,1)=xhat0; % initialize estimate
-P_hat(:,:,1)=P0;
-Sx(:,1)=sqrt(diag(P_hat(:,:,1)));
-for k=2:MAXK
-% simulate model dynamics by one time step
-u_true(k-1)=u_bar + sqrtm(S_u)*randn(1,1); % gaussian random noise with mean
-u_bar
-x_tru(:,k)=A_true*x_tru(:,k-1) + B_true*u_true(k-1); % true state change
-y(k)=C*x_tru(:,k) + sqrtm(S_v)*randn(1,1); % get a measurement (noisy)
-% time update the estimate
-x_hat(:,k)=A_model*x_hat(:,k-1); % the model assumes ubar=0
-P_hat(:,:,k)=A_model*P_hat(:,:,k-1)*A_model' + Q_model + Q_extra;
-% measurement update
-Sr(:,:,k)=C*P_hat(:,:,k)*C' + R; % innovations covariance
-K(:,k)=P_hat(:,:,k)*C'*inv(Sr(:,:,k));
-ry(:,k)=y(k)-C*x_hat(:,k); % innovations
-x_hat(:,k)=x_hat(:,k) + K(:,k)*ry(:,k);
-P_hat(:,:,k)=(eye(2) - K(:,k)*C)*P_hat(:,:,k);
-eta(k)=ry(:,k)'*inv(Sr(:,:,k))*ry(:,k);
-Sx(:,k)=sqrt(diag(P_hat(:,:,k)));
+clc;
+clear;
+%----------------------------problem 2-------------------------------------
+A_c = [0 0 1 0;0 0 0 1;-2 1 -1.6 0.8;1 -1 0.8 -0.8];
+B_c = [0 0;0 0;1 0;0 1];
+C_c = [0 1 0 0;0 0 0 1];
+dT = 0.1;
+A_d = expm(A_c * dT);
+B_d = (expm(A_c * dT) - eye(4))*inv(A_c)*B_c;
+sigma_u = [0.2 0;0 0.2];
+sigma_v = [0.1 0;0 0.1];
+%-------------------discrete time system simulation------------------------
+x0 =[1;2;0;0];
+x_k = x0;
+x_k1 = x_k;
+u=[0;0];
+n=0;
+xp = zeros(4,100);
+xp(:,1) = x_k;
+while(n<100)
+x_k = x_k1;
+x_k1 = A_d * x_k + B_d*u;
+xp(:,n+1) = x_k1;
+n=n+1;
 end
-e=x_tru-x_hat; % compute estimate error
-sig=sqrt([squeeze(P_hat(1,1,:))';squeeze(P_hat(2,2,:))']); % std dev of estimate error
-Sr=squeeze(Sr); % remove singleton dimensions
-% plot results
+x2_k = x0;
+x2_k1 = x2_k;
+n2=0;
+xp2 = zeros(4,100);
+xp2(:,1) = x2_k;
+while(n2<100)
+x2_k = x2_k1;
+urand = normrnd(0,0.2*eye(2));
+uk=[urand(1,1);urand(2,2)];
+x2_k1 = A_d * x2_k + B_d*uk;
+xp2(:,n2+1) = x2_k1;
+n2=n2+1;
+end
+t=0:0.1:10-0.1;
+h1 = plot(t, xp(1,:), '-', t, xp(2,:),'-.', t, xp2(1,:), '--', t, xp2(2,:),':');
+%h1 = plot(t, xp(1,:), '-', t, xp(2,:),'-.');
+xlabel('Time(sec)');
+ylabel('masses position');
+legend('mass 1','mass 2','mass 1 u~(0,0.2I)','mass 2 u~(0,0.2I)');
+%-----------------------continous system simulation------------------------
+x0 = [1;2;0;0];
+t0 = 0: 0.1: 10;
+zz = my_rk4(@spring_sys,t0,x0);
+zz2 = my_rk4(@spring_sys2,t0,x0);
 figure
-subplot(2,2,1)
-plot(time,e(1,:),'b',time,Sx(1,:),'b:',time,-Sx(1,:),'b:')
-xlim([t0 tf])
-ylabel('x_1')
-subplot(2,2,2)
-plot(time,e(2,:),'b',time,Sx(2,:),'b:',time,-Sx(2,:),'b:')
-xlim([t0 tf])
-ylabel('x_2')
-subplot(2,2,3)
-plot(1:MAXK,ry,'b',1:MAXK,sqrt(Sr),'b:',1:MAXK,-sqrt(Sr),'b:');
-xlim([0 200])
-ylim([-2 2])
-ylabel('innovations')
-text(10,1,'k=knom, b=bnom')
-subplot(2,2,4)
-plot(1:MAXK,eta)
-xlim([0 200])
-ylim([0 50])
-ylabel('eta')
-text(10,40,sprintf('mean(eta): %g',mean(eta)))
+h2 = plot(t0,zz(1,:),'-',t0,zz(2,:),'-.',t0,zz2(1,:),'--',t0,zz2(2,:),':' );
+
+xlabel('Time(sec)');
+ylabel('masses position');
+legend('mass 1','mass 2','mass 1 u~(0,0.2I)','mass 2 u~(0,0.2I)');
+% system model with u=[0 0]'
+function dx= spring_sys(t,x)
+u=[0;0];
+dx=[1;2;0;0];
+A_c = [0 0 1 0;0 0 0 1;-2 1 -1.6 0.8;1 -1 0.8 -0.8];
+B_c = [0 0;0 0;1 0;0 1];
+dx = A_c * x +B_c *u;
+end
+% system model with u ~ N(0, 0.2*eye(2))
+function dx= spring_sys2(t,x)
+urand = normrnd(0,0.2*eye(2));
+u=[urand(1,1);urand(2,2)];
+dx=[1;2;0;0];
+A_c = [0 0 1 0;0 0 0 1;-2 1 -1.6 0.8;1 -1 0.8 -0.8];
+B_c = [0 0;0 0;1 0;0 1];
+dx = A_c * x +B_c *u;
+end
+function YY = my_rk4(f,t0,x0)
+dt = t0(2)-t0(1);
+YY = zeros(length(x0),length(t0));
+xn = x0;
+YY(:,1)= xn;
+dx1 = f(dt, xn)*dt;
+xh1 = xn + dx1/2;
+dx2 = f(dt, xh1)*dt;
+xh2 = xn + dx2/2;
+dx3 = f(dt, xh2)*dt;
+xh3 = xn + dx3;
+dx4 = f(dt, xh3)*dt;
+xn1 = xn+(dx1+2*dx2+2*dx3+dx4)/6;
+flag = 1;
+while(flag<length(t0))
+xn = xn1;
+YY(:,flag+1) = xn;
+dx1 = f(dt, xn)*dt;
+xh1 = xn + dx1/2;
+dx2 = f(dt,xh1)*dt;
+xh2 = xn + dx2/2;
+dx3 = f(dt,xh2)*dt;
+xh3 = xn + dx3;
+dx4 = f(dt,xh3)*dt;
+xn1 = xn +(dx1+2*dx2+2*dx3+dx4)/6;
+flag = flag +1;
+end
+end
