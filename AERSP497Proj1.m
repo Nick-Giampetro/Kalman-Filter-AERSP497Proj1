46ed0210ae003511f8f52fe9038eaeae759562e3
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-10-12 21:45:40 -0400

    progress

--- AERSP497Proj1.m
+++ AERSP497Proj1.m
@@ -4,9 +4,20 @@ clear;
 close all;
 
 
-A_c = [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1; -2 1 0 -1.6 0.8 0; 1 -2 1 0.8 -1.6 0.8; 0 1 -1 0 0.8 -0.8];
-B_c = [0 0 0; 0 0 0; 0 0 0; 1 0 0; 0 1 0; 0 0 1];
-C = [1 0 0 0 0 0 ;0 1 0 0 0 0 ; 0 0 1 0 0 0];
+A_c = [0 0 0 1 0 0 ; 
+       0 0 0 0 1 0 ; 
+       0 0 0 0 0 1 ; 
+       -2 1 0 -1.6 0.8 0 ; 
+       1 -2 1 0.8 -1.6 0.8 ; 
+       0 1 -1 0 0.8 -0.8] ;
+B_c = [0 0 0 ; 
+       0 0 0 ; 
+       0 0 0 ; 
+       1 0 0 ;  
+       0 1 0 ; 
+       0 0 1] ;
+%C = [1 1 1 0 0 0] ;
+%C = [1 0 0 0 0 0 ;0 1 0 0 0 0 ; 0 0 1 0 0 0];
 %C = [0 1 0 0 0 0 ;0 0 0 1 0 0 ; 0 0 0 0 0 1];
 
 dT = 0.1;
@@ -20,14 +31,15 @@ x_k1 = x_k;
 x_hat = zeros(6,100);
 u_bar = 0 ;
 
-S_u = 0.2 * eye(3) ;
-S_v = 0.1 * eye(3) ;
+S_u = 0.2 ;
+S_v = 0.1 ;
+
 Q = B_d*S_u*B_d' ;
-R = eye(3)*S_v*eye(3)' ;
+R = 0.1*eye(3) ;
 
 P0 = blkdiag(eye(3),0.1*eye(3)) ;
 % P0 = zeros(6,6)' ;
-P_hat(:,:,1) = P0 ;
+P(:,:,1) = P0 ;
 xhat0 = x0 ;
 x_hat(:,1)= xhat0 ;
 x_tru(:,1)= x0 ;
@@ -38,33 +50,31 @@ for k = 2:100
     u_true(:,k-1)=[urandu(1,1);urandu(2,2);urandu(3,3)]; % gaussian random noise with mean S_u
     x_tru(:,k)=A_d*x_tru(:,k-1) + B_d*u_true(:,k-1); % true state change
     
-    %C = [1 0 0 0 0 0 ;0 1 0 0 0 0 ; 0 0 1 0 0 0];
+    C = [1 0 0 0 0 0 ;0 1 0 0 0 0 ; 0 0 1 0 0 0];
   
     urandv = normrnd(u_bar,S_v*eye(3)) ;
     u_sensor(:,k-1)= [urandv(1,1);urandv(2,2);urandv(3,3)] ;
     y(:,k-1) = C*x_tru(:,k) + sqrtm(S_v)*u_sensor(:,k-1);
 
-    x_hat(:,k) = A_d*x_hat(:,k-1); % the model assumes ubar=0
-    P_hat(:,:,k) = A_d*P_hat(:,:,k-1)*A_d' + Q;
-    
-    %C = [0 1 1 1 1 1 ; 1 0 1 1 1 1 ; 1 1 0 1 1 1];
+    x_hat(:,k) = A_d*x_hat(:,k-1) ; % the model assumes ubar=0
+    P(:,:,k) = A_d*P(:,:,k-1)*A_d' + Q;
 
     % measurement update
-    P_hat(:,:,k) = inv(inv(P_hat(:,:,k)) + C'*inv(R)*C) ;
-    K(:,:,k) = P_hat(:,:,k)*C'*inv(R) ;
-    x_hat(:,k)=x_hat(:,k) + K(:,:,k)*(y(k) - C*x_hat(:,k)) ;
+    K(:,:,k)=P(:,:,k)*C'*inv(C*P(:,:,k)*C' + R);
+    x_hat(:,k)=x_hat(:,k) + K(:,:,k)*(y(:,k-1) - C*x_hat(:,k));
+    P(:,:,k)=(eye(6) - K(:,:,k)*C)*P(:,:,k);
     
     % now compute estimate error
     e(:,k)=x_tru(:,k)-x_hat(:,k);
-    trP(k)=trace(P_hat(:,:,k));
-    Sx(:,k)=sqrt(diag(P_hat(:,:,k)));
+    trP(k)=trace(P(:,:,k));
+    Sx(:,k)=sqrt(diag(P(:,:,k)));
 end
 
 urandv = normrnd(u_bar,S_v*eye(3)) ;
 u_sensor(:,100)= [urandv(1,1);urandv(2,2);urandv(3,3)] ;
 y(:,100) = C*x_tru(:,k) + sqrtm(S_v)*u_sensor(:,k-1);
 
-sig=sqrt([squeeze(P_hat(1,1,:))';squeeze(P_hat(2,2,:))']);
+sig=sqrt([squeeze(P(1,1,:))';squeeze(P(2,2,:))']);
 
 t=0:dT:10-dT;
 
