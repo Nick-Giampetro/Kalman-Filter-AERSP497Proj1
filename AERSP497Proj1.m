08e12603411dc7c8bbc30ed1742f08640d289b87
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-10-13 01:08:14 -0400

    graphs

--- AERSP497Proj1.m
+++ AERSP497Proj1.m
@@ -62,32 +62,19 @@ for rn = 1:50
         
         % now compute estimate error
         e(:,k)=x_tru(:,k)-x_hat(:,k);
-        P_trace(rn,k)=sqrt(trace(P(:,:,k)));
         e_norm(rn,k) = norm(e(:,k));
+        P_trace(rn,k)=sqrt(trace(P(:,:,k)));
     end
     
     urandv = normrnd(u_bar,S_v*eye(3)) ;
     u_sensor(:,100)= [urandv(1,1);urandv(2,2);urandv(3,3)] ;
-    y(:,100) = C*x_tru(:,k) + sqrtm(S_v)*u_sensor(:,k-1);
+    y(:,100) = C*x_tru(:,k) + sqrtm(S_v)*u_sensor(:,k-1) ;
 end
 
 t=0:dT:10-dT;
 
 figure
-h0 = plot(t, y(1,:),'r',t, y(2,:),'g',t, y(3,:),'b');
+h0 = plot(t,mean(e_norm(:,:)),t,mean(P_trace(:,:)));
 xlabel('Time(sec)');
-ylabel('Mass positions according to sensor');
-legend('mass 1 u~(0,0.1I)','mass 2 u~(0,0.1I)','mass 3 u~(0,0.1I)');
-
-figure
-h1 = plot(t, x_tru(1,:), '--', t, x_tru(2,:),'--',t, x_tru(3,:), '--', t, x_hat(1,:),'r',t, x_hat(2,:),'g',t, x_hat(3,:),'b');
-xlabel('Time(sec)');
-ylabel('Mass positions according to dynamics');
-legend('mass 1 u~(0,0.2I)','mass 2 u~(0,0.2I)','mass 3 u~(0,0.2I)','mass 1 (KF)','mass 2 (KF)','mass 3 (KF)');
-
-figure
-h2 = plot(t,e(1,:),'r',t,e(2,:),'g',t,e(3,:),'b') ;
-xlabel('Time(sec)');
-ylabel('Error');
-legend('mass 1','mass 2','mass 3');
-
+ylabel('Average of errors at each step in time');
+legend('Avg error','P Trace');
